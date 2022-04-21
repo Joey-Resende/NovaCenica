570a14b843bcd775dec52e7163df72664cc7edcf
Joey-Resende <givanildo.resende@gmail.com>
2022-04-21 07:57:55 -0300

    Adicionando mais um view e template ao projeto

--- novaCenica/core/urls.py
+++ novaCenica/core/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import HomeView
+from .views import HomeView, TubeView
 
 urlpatterns = [
     path('home/', HomeView.as_view(), name='home'),
+    path('TubeCenica/', TubeView.as_view(), name='TubeCenica'),
 ]
--- novaCenica/core/views.py
+++ novaCenica/core/views.py
@@ -2,4 +2,8 @@ from django.views.generic import TemplateView
 
 
 class HomeView(TemplateView):
-    template_name = "index.html"
+    template_name = 'base_model.html'
+
+
+class TubeView(TemplateView):
+    template_name = 'TubeCenica.html'
